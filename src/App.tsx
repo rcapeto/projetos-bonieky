f6ccc0d531a429c8ea4357c27d9e9c2d15834e79
Raphael Capeto <raphaelcapeto@gmail.com>
2021-10-10 15:43:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import SistemaFinanceiro from './projects/SistemaFinanceiro';
+
+function App() {
+  return (
+    <div className="App">
+      <SistemaFinanceiro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/projects/SistemaFinanceiro/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SistemaFinanceiro() {
+   return(
+      <div>
+         Sistema Financeiro
+      </div>
+   );
+}
